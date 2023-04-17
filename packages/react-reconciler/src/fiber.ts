e61397ecfa95a56027c33d7e871be146777a215d
lwrench <1243115181@qq.com>
2023-04-18 00:35:22 +0800

    feat: updatequeue

--- packages/react-reconciler/src/fiber.ts
+++ packages/react-reconciler/src/fiber.ts
@@ -17,8 +17,10 @@ export class FiberNode {
 	index: number;
 
 	memoizedProps: Props | null;
+	memoizedState: any;
 	alternate: FiberNode | null;
 	flags: FlagsType;
+	updateQueue: unknown;
 
 	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
 		this.tag = tag;
@@ -39,6 +41,8 @@ export class FiberNode {
 		// 作为工作单元
 		this.pendingProps = pendingProps;
 		this.memoizedProps = null;
+		this.updateQueue = null;
+		this.memoizedState = null;
 
 		this.alternate = null;
 		// 副作用
@@ -58,3 +62,30 @@ export class FiberRootNode {
 		this.finishedWork = null;
 	}
 }
+
+export const createWorkInProgress = (
+	current: FiberNode,
+	pendingProps: Props
+): FiberNode => {
+	let wip = current.alternate;
+
+	if (wip === null) {
+		// mount
+		wip = new FiberNode(current.tag, pendingProps, current.key);
+		wip.stateNode = current.stateNode;
+
+		wip.alternate = current;
+		current.alternate = wip;
+	} else {
+		// update
+		wip.pendingProps = pendingProps;
+		wip.flags = NoFlags;
+	}
+	wip.type = current.type;
+	wip.updateQueue = current.updateQueue;
+	wip.child = current.child;
+	wip.memoizedProps = current.memoizedProps;
+	wip.memoizedState = current.memoizedState;
+
+	return wip;
+};
