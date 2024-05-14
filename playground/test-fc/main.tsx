ee5dc6d7be135a75aa7715f92f50dbf7170f277b
lwrench <1243115181@qq.com>
2024-05-14 21:52:34 +0800

    test: array diff

--- playground/test-fc/main.tsx
+++ playground/test-fc/main.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
 	const [num, setNum] = useState(100);
 
-	return <div onClick={() => setNum(num + 1)}>{num}</div>;
-}
-function Child() {
-	return <span>Hello React</span>;
+	const arr =
+		num % 2 === 0
+			? [<li key="1">1</li>, <li key="2">2</li>, <li key="3">3</li>]
+			: [<li key="3">3</li>, <li key="2">2</li>, <li key="1">1</li>];
+
+	return <ul onClickCapture={() => setNum(num + 1)}>{arr}</ul>;
 }
 
-ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
+function Child() {
+	return <span>big-react</span>;
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<App />
 );
